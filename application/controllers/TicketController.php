d56b89e5f3f26a8fe1ba5268170aed58704d150a
alex <tangyongjin@gmail.com>
2017-03-06 03:16:34 +0800

    route-server query Done! expect bug fiexed

---
+++ application/controllers/TicketController.php
@@ -0,0 +1,118 @@
+<?php
+
+        
+class TicketController extends IXP_Controller_FrontEnd
+{
+    /**
+     * This function sets up the frontend controller
+     */
+    protected function _feInit()
+    {
+      //  $this->assertPrivilege( \Entities\User::AUTH_SUPERUSER );
+    
+        $this->view->feParams = $this->_feParams = (object)[
+            //'entity'        => '\\Entities\\Ticket',
+             'form'          => 'IXP_Form_ContactGroup',
+            'pagetitle'     => 'Ticket',
+        
+            'titleSingular' => 'Ticket',
+            'nameSingular'  => 'Ticket',
+        
+            'defaultAction' => 'list',                    // OPTIONAL; defaults to 'list'
+        
+            'listOrderBy'    => 'id',
+            'listOrderByDir' => 'DESC',
+        
+            'listColumns'    => [
+                'id'        => [ 'title' => '序列号', 'display' => true ],
+               
+
+             'pingdate'       => [
+                    'title'     => 'pingdate',
+                    'type'      => self::$FE_COL_TYPES[ 'DATETIME' ]
+                ],
+
+
+                'custname'      => 'custname',
+                'sub_ip' =>'sub_ip',
+                'packetloss'=>'packetloss',
+                'min_t'=>'min_t',
+                'max_t'=>'max_t',
+                'avg_t'=>'avg_t',
+            ]
+        ];
+    
+        // display the same information in the view as the list
+        $this->_feParams->viewColumns = array_merge(
+            $this->_feParams->listColumns,
+            [
+                'active'      => 'Active',
+                'limit'       => 'Limit',
+                'description' => 'Description'
+            ]
+        );
+    }
+    
+    
+
+ 
+    /**
+     * Provide array of objects for the listAction and viewAction
+     *
+     * @param int $id The `id` of the row to load for `viewAction`. `null` if `listAction`
+     */
+    protected function listGetData( $id = null )
+    {
+ 
+
+
+        $conn = $this->getD2EM()->getConnection();
+
+        $sql = " select IF(packetloss>0,'red', 'black') as color,id,pingdate,id as custname,sub_ip,packetloss,min_t,max_t,avg_t   ";
+        $sql.= " from batchping order by id desc limit 100 ";
+        $stmt = $conn->prepare($sql);
+         
+        $stmt->execute();
+        return  $stmt->fetchAll();
+ 
+  
+    }
+    
+     /**
+     * Post process hook that can be overridden by subclasses for add and edit actions.
+     *
+     * This is called immediately after the initstantiation of the form object and, if
+     * editing, includes the Doctrine2 entity `$object`.
+     *
+     * If you need to have, for example, edit values set in the form, then use the
+     * `addPrepare()` hook rather than this one.
+     *
+     * @see addPrepare()
+     * @param OSS_Form $form The form object
+     * @param \Entities\ContactGroup $object The Doctrine2 entity (being edited or blank for add)
+     * @param bool $isEdit True of we are editing an object, false otherwise
+     * @param array $options Options passed onto Zend_Form
+     * @param string $cancelLocation Where to redirect to if 'Cancal' is clicked
+     */
+    protected function formPostProcess( $form, $object, $isEdit, $options = null, $cancelLocation = null )
+    {
+        return;
+        //$form->getElement( 'type' )->setMultiOptions( $this->_options['contact']['group']['types'] );
+    }
+    
+    
+    /**
+     * Postvalidation hook for add / edit
+     *
+     * @param OSS_Form $form The Send form object
+     * @param \Entities\ContactGroup $group The Doctrine2 contact group entity
+     * @param bool $isEdit True if we are editing, otherwise false
+     * @return bool If false, the form is not processed
+     */
+    protected function addPostValidate( $form, $group, $isEdit )
+    {
+        return false;
+     }
+    
+}
+
--- var/templates_c/87d220c75bcf45f3416bb37ea8470f39158dc1f2.file.header-documentation.phtml.php
+++ var/templates_c/87d220c75bcf45f3416bb37ea8470f39158dc1f2.file.header-documentation.phtml.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.19-dev, created on 2017-03-02 02:39:22
+<?php /* Smarty version Smarty-3.1.19-dev, created on 2017-03-03 12:04:11
          compiled from "/opt/ixpmanager/application/views/header-documentation.phtml" */ ?>
 <?php /*%%SmartyHeaderCode:12827255558b7155aaee857-42158566%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '87d220c75bcf45f3416bb37ea8470f39158dc1f2' => 
     array (
       0 => '/opt/ixpmanager/application/views/header-documentation.phtml',
-      1 => 1488392703,
+      1 => 1488513847,
       2 => 'file',
     ),
   ),
@@ -15,9 +15,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.19-dev',
   'unifunc' => 'content_58b7155aafee18_07781221',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_58b7155aafee18_07781221')) {function content_58b7155aafee18_07781221($_smarty_tpl) {?><?php if (!is_callable('smarty_function_genUrl')) include '/opt/ixpmanager/application/../vendor/opensolutions/oss-framework/src/OSS/Smarty/functions/function.genUrl.php';
 ?><li class="dropdown">
@@ -32,10 +32,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 
         <li>
             <a href="<?php echo smarty_function_genUrl(array('controller'=>"batch-ping",'action'=>"list"),$_smarty_tpl);?>
-">BatchPing(Underbuild...)</a>
+">BatchPing</a>
         </li>
-
-
 
 
         <li>
@@ -44,8 +42,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
         </li>
 
         <li>
-            <a href="<?php echo smarty_function_genUrl(array('controller'=>"static",'action'=>"example"),$_smarty_tpl);?>
-">Feature1(under build..)</a>
+            <a href="<?php echo smarty_function_genUrl(array('controller'=>"ticket",'action'=>"list"),$_smarty_tpl);?>
+">故障(under build..)</a>
         </li>
 
 
